48e46f628f766e200dcccae63e68952f8d9d7574
andrew chen <andrewxinchen@gmail.com>
2020-05-03 17:03:05 -0400

    cleaned up repo and added minor documentation

--- popVariable.py
+++ popVariable.py
@@ -1,3 +1,5 @@
+#solves inverse SIR problem with population as a variable (provides effective susceptible population estimate)
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
